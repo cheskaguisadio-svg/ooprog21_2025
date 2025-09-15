9ddfbf304d99ef64ee2eed0919832e84fc71a9aa
cheskaguisadio-svg <cheskaguisadio@gmail.com>
2025-09-15 09:06:19 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,12 @@
+public class CompanyInfo {
+    public static void displayHours() {
+        System.out.println("Monday-Friday 8am to 6pm");
+        System.out.println("Saturday am to noon");
+        System.out.println("sunday closed");
+    }
+    public static void main(String[] args){
+    System.out.println("Smart Electronics");
+    
+    displayHours();
+     }
+}
