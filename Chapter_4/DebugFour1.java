212888a6cffe5872bbcd43d6241d9a79da0abc52
cheskaguisadio-svg <cheskaguisadio@gmail.com>
2025-09-29 08:31:42 +0800

    Update DebugFour1.java

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class DebugFour1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int radius;
+
+        System.out.print("Enter a radius for a circle >> ");
+        radius = input.nextInt();
+
+        DebugCircle c = new DebugCircle(radius);
+
+        System.out.println("The radius is " + c.getRadius());
+        System.out.println("The diameter is " + c.getDiameter());
+        System.out.println("The area is " + c.getArea());
+    }
+}
