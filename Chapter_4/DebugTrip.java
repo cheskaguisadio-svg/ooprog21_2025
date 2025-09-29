5c8f609ca583b6fe4ad5f8221ddec63082be2861
cheskaguisadio-svg <cheskaguisadio@gmail.com>
2025-09-29 08:08:44 +0800

    Update DebugTrip.java

--- Chapter_4/DebugTrip.java
+++ Chapter_4/DebugTrip.java
@@ -1,48 +1,48 @@
-public class DebugTrip {
+public class DebugTrip
+{
     private String destination;
     private String departure;
     private String mode;
-
     private static final String DEFAULT_CITY = "Atlanta";
     private static final String DEFAULT_MODE = "car";
-
     
-    public DebugTrip(String destination) {
+    public DebugTrip(String destination)
+    {
         this(destination, DEFAULT_CITY, DEFAULT_MODE);
     }
-
- 
-    public DebugTrip(String destination, String departure) {
+    
+    public DebugTrip(String destination, String departure)
+    {
         this(destination, departure, DEFAULT_MODE);
     }
-
     
-    public DebugTrip(String destination, String departure, String mode) {
+    public DebugTrip(String destination, String departure, String mode)
+    {
         this.destination = destination;
         this.departure = departure;
         this.mode = mode;
     }
-
-    public String getDestination() {
+    
+    public String getDestination()
+    {
         return destination;
     }
-
-    public String getDepartureCity() {
+    
+    public String getDepartureCity()
+    {
         return departure;
     }
-
-    public String getMode() {
+    
+    public String getMode()
+    {
         return mode;
     }
+}
 
 
-    public static void main(String[] args) {
-        DebugTrip trip1 = new DebugTrip("New York");
-        DebugTrip trip2 = new DebugTrip("Los Angeles", "Chicago");
-        DebugTrip trip3 = new DebugTrip("Miami", "Houston", "plane");
 
-        System.out.println("Trip 1: Destination = " + trip1.getDestination() + ", Departure = " + trip1.getDepartureCity() + ", Mode = " + trip1.getMode());
-        System.out.println("Trip 2: Destination = " + trip2.getDestination() + ", Departure = " + trip2.getDepartureCity() + ", Mode = " + trip2.getMode());
-        System.out.println("Trip 3: Destination = " + trip3.getDestination() + ", Departure = " + trip3.getDepartureCity() + ", Mode = " + trip3.getMode());
-    }
+
+
+
+
 }
