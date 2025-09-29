03a6c22ee9f9f91d23a0ad3b2a242eccde31895d
cheskaguisadio-svg <cheskaguisadio@gmail.com>
2025-09-29 08:33:15 +0800

    Update DebugFour3.java

--- Chapter_4/DebugFour3.java
+++ Chapter_4/DebugFour3.java
@@ -1,1 +1,37 @@
 
+import java.util.Scanner;
+
+public class DebugFour3
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        int w, l, h;
+
+        System.out.print("Enter width of box >> ");
+        w = input.nextInt();
+        System.out.print("Enter length of box >> ");
+        l = input.nextInt();
+        System.out.print("Enter height of box >> ");
+        h = input.nextInt();
+
+        DebugBox box1 = new DebugBox();
+        DebugBox box2 = new DebugBox(w, l, h);
+
+        System.out.println("\nThe dimensions of the first box are:");
+        box1.showData();
+        System.out.print("The volume of the first box is: ");
+        showVolume(box1);
+
+        System.out.println("\nThe dimensions of the second box are:");
+        box2.showData();
+        System.out.print("The volume of the second box is: ");
+        showVolume(box2);
+    }
+
+    public static void showVolume(DebugBox aBox)
+    {
+        double vol = aBox.getVolume();
+        System.out.println(vol);
+    }
+}
