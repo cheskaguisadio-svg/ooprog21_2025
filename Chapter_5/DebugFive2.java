5ede5c3be58bff38b3e3af503f6ea2365176283e
cheskaguisadio-svg <cheskaguisadio@gmail.com>
2025-10-08 14:22:50 +0800

    Create DebugFive2.java

---
+++ Chapter_5/DebugFive2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DebugFive2 {
+    public static void main(String args[]) {
+        int num;
+        int num2;
+        Scanner input = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        num = input.nextInt();  // Corrected to nextInt()
+
+        System.out.print("Enter another number: ");
+        num2 = input.nextInt();  // Corrected to nextInt()
+
+       
+        if ((num % num2 == 0) || (num2 % num == 0)) {  // Corrected the logic
+            System.out.println("One of these numbers is evenly divisible into the other.");
+        } else {
+            System.out.println("Neither of these numbers is evenly divisible into the other.");
+        }
+    }
+}
